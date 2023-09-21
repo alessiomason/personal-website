d008dbb98e5b7620a4825600a27cf802eed091d5
Alessio Mason <alessio.mason@me.com>
2023-09-21 18:25:52 +0200

    2nd home (initial central boxes); custom backgrounds

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import ReactPageScroller from 'react-page-scroller';
-import { ArrowDown, Github, Mastodon } from 'react-bootstrap-icons';
+import { ArrowDown, Laptop, Phone, FiletypeJsx, Server, FiletypePhp, Apple, Android, HourglassSplit, Github, Mastodon } from 'react-bootstrap-icons';
 import { useInView } from 'react-intersection-observer';
 import './Global.css';
 import './Home.css';
@@ -94,19 +94,38 @@ function SecondHomeSlide() {
       </div>
 
       <Row>
-        <Col sm={{span: 4, offset: 1}}>
-          <h3 className='text-center'>Ambiti (line around)</h3>
+        <Col sm={{ span: 4, offset: 1 }} className='border-line-around-animation'>
+          <h3 className='text-center'>Interessi e competenze</h3>
+          <ul className='first-ul'>
+            <li className='first-li'><span className='d-flex align-items-center'><Laptop className='list-icon' />Applicazioni web</span>
+              <ul>
+                <li><span className='d-flex align-items-center'><FiletypeJsx className='list-icon' />Front-end: React</span></li>
+                <li><span className='d-flex align-items-center'><Server className='list-icon' />Back-end: Express o Spring</span></li>
+                <li><span className='d-flex align-items-center'><FiletypePhp className='list-icon' />Programmazione web tradizionale</span></li>
+              </ul>
+            </li>
+
+            <li className='first-li'><span className='d-flex align-items-center'><Phone className='list-icon' />Applicazioni mobile</span>
+              <ul>
+                <li><span className='d-flex align-items-center'><Apple className='list-icon' />Applicazioni iOS (Swift, SwiftUI)</span></li>
+                <li><span className='d-flex align-items-center'><Android className='list-icon' />Android (Kotlin, view-based o Jetpack Compose)</span></li>
+              </ul>
+            </li>
+          </ul>
         </Col>
 
-        <Col sm={{span: 4, offset: 2}}>
-          <h3 className='text-center'>Lavori (line around)</h3>
+        <Col sm={{ span: 4, offset: 2 }} className='border-line-around-animation'>
+          <h3 className='text-center'>Lavori e progetti</h3>
+          <span className='d-flex justify-content-center'><HourglassSplit className='waiting-icon' /></span>
+          <p>Il sito è ancora parzialmente in costruzione: troverai presto alcuni dei miei lavori e progetti personali in una pagina dedicata.</p>
+          <p>Su <a href='https://github.com/alessiomason' className='text-decorate-link'>GitHub</a> trovi alcuni dei miei progetti e (presto) anche il repository di questo stesso sito.</p>
         </Col>
       </Row>
 
       <div ref={ref} className={inView ? 'flicker-animation' : 'transparent'}>
-        <h3>In giro per il web</h3>
-        <div className='d-flex align-items-center'><Github /><a href='https://github.com/alessiomason'>@alessiomason</a></div>
-        <div className='d-flex align-items-center'><Mastodon /><a href='https://livellosegreto.it/@alemason'>@alemason@livellosegreto.it</a></div>
+        <h3>In giro per il web (Across the Spider-Verse)</h3>
+        <div className='d-flex align-items-center'><Github className='web-icon' /><a href='https://github.com/alessiomason'>@alessiomason</a></div>
+        <div className='d-flex align-items-center'><Mastodon className='web-icon' /><a href='https://livellosegreto.it/@alemason'>@alemason@livellosegreto.it</a></div>
       </div>
     </div>
   );
--- src/Maintenance/Maintenance.jsx
+++ src/Maintenance/Maintenance.jsx
@@ -4,17 +4,17 @@ import '../Global.css';
 
 function Maintenance() {
   return (
-    <div className="d-flex flex-column justify-content-center full-screen-slide">
-      <div className="d-flex center-row no-padding">
-        <img src={Klink} className="klink" alt="loading" />
+    <div className='d-flex flex-column justify-content-center full-screen-slide maintenance-page'>
+      <div className='d-flex center-row no-padding'>
+        <img src={Klink} className='klink' alt='loading' />
       </div>
 
       <div>
-        <h2 className="text-center">Alessio Mason</h2>
+        <h2 className='text-center primary-yellow'>A</h2>
       </div>
 
       <div>
-        <h4 className="text-center">This website is currently under construction. Check back later!</h4>
+        <h4 className='text-center primary-yellow'>This website is currently under construction. Check back later!</h4>
       </div>
     </div>
   );
