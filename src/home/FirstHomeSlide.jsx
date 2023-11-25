39775bfb6e82f60a4327b31a02182b36c28cb96a
Alessio Mason <alessio.mason@me.com>
2023-11-25 13:02:32 +0100

    Festivities message in PieceOfCode

--- src/home/FirstHomeSlide.jsx
+++ src/home/FirstHomeSlide.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { ArrowDown } from 'react-bootstrap-icons';
 import { useMediaQuery } from 'react-responsive';
+import PieceOfCode from './PieceOfCode';
 import RandomQuote from './RandomQuote';
 import ProfilePicture from '../images/profile_picture.jpeg';
 import './FirstHomeSlide.css';
-const dayjs = require('dayjs');
 
 function FirstHomeSlide() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -26,50 +25,6 @@ function FirstHomeSlide() {
       </Row>
 
       <ArrowDown className='arrow-down' />
-    </div>
-  );
-}
-
-function PieceOfCode() {
-  const [dirty, setDirty] = useState(false);
-  const [time, setTime] = useState(dayjs().format());
-  const [spinner, setSpinner] = useState('|');
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDirty(true);
-    }, 100)
-
-    return () => clearInterval(intervalId);
-  }, [])
-
-  useEffect(() => {
-    if (dirty) {
-      setTime(dayjs().format());
-
-      setSpinner(currentSpinner => {
-        switch (currentSpinner) {
-          case '|':
-            return '/';
-          case '/':
-            return '-';
-          case '-':
-            return '\\';
-          case '\\':
-          default:
-            return '|';
-        }
-      })
-
-      setDirty(false);
-    }
-  }, [dirty])
-
-  return (
-    <div className='code'>
-      <p className='text-end no-padding primary-yellow'>const dayjs = require('dayjs');</p>
-      <p className='text-end no-padding primary-yellow'>console.log(dayjs().format())</p>
-      <p className='text-end no-padding primary-yellow'>{spinner} {time}</p>
     </div>
   );
 }
---
+++ src/home/PieceOfCode.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import festivities from './Festivities';
+import './PieceOfCode.css';
+const dayjs = require('dayjs');
+
+function PieceOfCode() {
+  const [dirty, setDirty] = useState(false);
+  const [time, setTime] = useState(dayjs().format());
+  const [spinner, setSpinner] = useState('|');
+  const [currentFestivity, setCurrentFestivity] = useState({});
+
+  useEffect(() => {
+    const now = dayjs();
+
+    for (const festivity of festivities) {
+      if (dayjs(festivity.start).isBefore(now) && dayjs(festivity.end).isAfter(now)) {
+        setCurrentFestivity(festivity);
+        break;
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDirty(true);
+    }, 100)
+
+    return () => clearInterval(intervalId);
+  }, [])
+
+  useEffect(() => {
+    if (dirty) {
+      setTime(dayjs().format());
+
+      setSpinner(currentSpinner => {
+        switch (currentSpinner) {
+          case '|':
+            return '/';
+          case '/':
+            return '-';
+          case '-':
+            return '\\';
+          case '\\':
+          default:
+            return '|';
+        }
+      })
+
+      setDirty(false);
+    }
+  }, [dirty])
+
+  return (
+    <div className='code'>
+      <p className='text-end no-padding primary-yellow'>const dayjs = require('dayjs');</p>
+      <p className='text-end no-padding primary-yellow'>console.log(dayjs().format())</p>
+      <p className='text-end no-padding primary-yellow'>{spinner} {time}</p>
+      {currentFestivity.message && <p className='text-end no-padding primary-yellow'>console.log({currentFestivity.message})</p>}
+    </div>
+  );
+}
+
+export default PieceOfCode;
