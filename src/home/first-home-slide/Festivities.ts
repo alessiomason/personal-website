0e434e44b3d60efd0b13143d2f7033cc705d74ea
Alessio Mason <alessio.mason@me.com>
2025-07-10 15:32:26 +0200

    Dynamically-built festivities

--- src/home/first-home-slide/Festivities.ts
+++ src/home/first-home-slide/Festivities.ts
@@ -1,5 +1,6 @@
-// start field is inclusive, end field is exclusive
+import dayjs from "dayjs";
 
+// Start field is inclusive, end field is exclusive.
 export class Festivity {
     start: string
     end: string
@@ -13,9 +14,25 @@ export class Festivity {
 
 }
 
-export const festivities = [
-    new Festivity("2025-12-16", "2025-12-30", "festivities-greetings.christmas"),
-    new Festivity("2025-12-30", "2026-01-03", "festivities-greetings.new-year"),
-    new Festivity("2025-04-17", "2025-04-22", "festivities-greetings.easter"),
-    new Festivity("2025-09-23", "2025-09-24", "festivities-greetings.birthday"),
-]
+const currentYear = parseInt(dayjs().format("YYYY"));
+let festivities: Festivity[] = [];
+
+// add recurring Italian holidays through the next 10 years
+for (let year = currentYear; year <= currentYear + 10; year++) {
+    const newYear = new Festivity(`${year - 1}-12-30`, `${year}-01-03`, "festivities-greetings.new-year");
+    const birthday = new Festivity(`${year}-09-23`, `${year}-09-24`, "festivities-greetings.birthday");
+    const christmas = new Festivity(`${year}-12-16`, `${year}-12-30`, "festivities-greetings.christmas");
+
+    festivities.push(newYear, birthday, christmas);
+}
+
+// add Easter
+festivities.push(
+    new Festivity("2026-04-02", "2026-04-07", "festivities-greetings.easter"),
+    new Festivity("2027-03-25", "2027-03-30", "festivities-greetings.easter"),
+    new Festivity("2028-04-13", "2028-04-18", "festivities-greetings.easter"),
+    new Festivity("2029-03-29", "2029-04-03", "festivities-greetings.easter"),
+    new Festivity("2030-04-18", "2030-04-23", "festivities-greetings.easter")
+);
+
+export {festivities};
